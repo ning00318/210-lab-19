64bb4cb1d80f7829d3ba8d2846d12f556fe39cee
NINA <ninatsao@ningningning.lan>
2024-10-06 15:35:25 -0700

    3rd commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-// COMSC-210 | Lab 19: Abstract & Automate Lab 18
+// COMSC-210 | Lab 19: Abstract & Automate Lab 18 | Nina Tsao
 
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 struct Node {
@@ -17,38 +18,41 @@ public:
     Movie(string t) {title = t;}
 
     void outputReviews(Node *head) {
-    cout << "Outputting all reviews:" << endl;
-    Node *current = head;
-    int i = 0;
-    float total = 0.0;
+        cout << "Outputting all reviews:" << endl;
+        Node *current = head;
+        int i = 0;
+        float total = 0.0;
 
-    while (current != nullptr) {    // while current is not empty, same as while(!current)
-        cout << "\t> Review #" << ++i << ": ";
-        cout << current->rating << ": " << current->comments << endl;
+        while (current != nullptr) {    // while current is not empty, same as while(!current)
+            cout << "\t> Review #" << ++i << ": ";
+            cout << current->rating << ": " << current->comments << endl;
 
-        total += current->rating;   // calculate the total rating
-        current = current->next;    // set the next node to current and review the data
+            total += current->rating;   // calculate the total rating
+            current = current->next;    // set the next node to current and review the data
+        }
+        
+        float average = total / i;
+        cout << "\t> Average: " << average << endl;
     }
-    
-    float average = total / i;
-    cout << "\t> Average: " << average << endl;
-}
 
     Node *addNodesToHead(Node *head, float rating, string comments) {
-    Node *newNode = new Node;       // add new node
-    newNode->rating = rating;       // add rating in new node
-    newNode->comments = comments;   // add comments in new node
-    newNode->next = head;           // add new node in head
+        Node *newNode = new Node;       // add new node
+        newNode->rating = rating;       // add rating in new node
+        newNode->comments = comments;   // add comments in new node
+        newNode->next = head;           // add new node in head
 
-    return newNode;     // return new head to the new node
+        return newNode;     // return new head to the new node
     }
-
-
 };
 
 int main() {
+    Movie movie;
+
+    ifstream fin;
+    fin.open("comments.txt");
+    
     Node *head = nullptr;   // initialize
-    outputReviews(head);     // output the data after entered all the informations
+    movie.outputReviews(head);     // output the data after entered all the informations
 
     return 0;
 }
