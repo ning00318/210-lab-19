3dceb1b885e7fccb86e3105da8e485ee5b85480f
NINA <ninatsao@ningningning.lan>
2024-10-06 15:21:20 -0700

    2nd commit

--- main.cpp
+++ main.cpp
@@ -15,86 +15,8 @@ private:
 
 public:
     Movie(string t) {title = t;}
-};
 
-// prototypes
-Node *addNodesToHead(Node*, float, string);
-Node *addNodesToTail(Node*, float, string);
-void outputReviews(Node*);
-
-int main() {
-    Node *head = nullptr;   // initialize
-    int choice;
-    float rating;
-    string comments;
-    string yn;
-
-    do {
-        cout << "Which linked list method should we use?" << endl;
-        cout << "\t[1] New nodes are added at the head of the linked list" << endl;
-        cout << "\t[2] New nodes are added at the tail of the linked list" << endl;
-        cout << "\tChoice: ";
-        cin >> choice;
-        cin.ignore();
-
-        if (choice == 1 || choice == 2) {
-            do {
-                cout << "Enter review rating 0-5: ";
-                cin >> rating;
-                cin.ignore();
-
-                cout << "Enter review comments: ";
-                getline(cin, comments);
-
-                if (choice == 1)
-                    head = addNodesToHead(head, rating, comments);
-                else
-                    head = addNodesToTail(head, rating, comments);
-                
-                cout << "Enter another review? Y/N: ";
-                getline(cin, yn);
-
-            } while (yn == "y" || yn == "Y");   // repeat while the answer is y or Y
-        }
-        else
-            cout << "Please enter choices correctly (option 1 or 2) and try again" << endl << endl;
-    
-    } while (choice != 1 && choice != 2);   // repeat while the choice is not equal to 1 and 2
-
-    outputReviews(head);     // output the data after entered all the informations
-
-    return 0;
-}
-
-Node *addNodesToHead(Node *head, float rating, string comments) {
-    Node *newNode = new Node;       // add new node
-    newNode->rating = rating;       // add rating in new node
-    newNode->comments = comments;   // add comments in new node
-    newNode->next = head;           // add new node in head
-
-    return newNode;     // return new head to the new node
-}
-
-Node *addNodesToTail(Node *head, float rating, string comments) {
-    Node *newNode = new Node;       // add new node
-    newNode->rating = rating;       // add rating in new node
-    newNode->comments = comments;   // add comments in new node
-    newNode->next = nullptr;        // means it's the last node
-
-    if (head == nullptr) {      // if head is null
-        head = newNode;         // make new node be the head
-        return head;
-    }
-
-    Node *current = head;               // make head be the current 
-    while (current->next != nullptr) {  // while current-> is not null,
-        current = current->next;        // move to the next node until reach the last one
-    }
-    current->next = newNode;            // make new node to the last
-    return head;
-}
-
-void outputReviews(Node *head) {
+    void outputReviews(Node *head) {
     cout << "Outputting all reviews:" << endl;
     Node *current = head;
     int i = 0;
@@ -110,4 +32,23 @@ void outputReviews(Node *head) {
     
     float average = total / i;
     cout << "\t> Average: " << average << endl;
+}
+
+    Node *addNodesToHead(Node *head, float rating, string comments) {
+    Node *newNode = new Node;       // add new node
+    newNode->rating = rating;       // add rating in new node
+    newNode->comments = comments;   // add comments in new node
+    newNode->next = head;           // add new node in head
+
+    return newNode;     // return new head to the new node
+    }
+
+
+};
+
+int main() {
+    Node *head = nullptr;   // initialize
+    outputReviews(head);     // output the data after entered all the informations
+
+    return 0;
 }
